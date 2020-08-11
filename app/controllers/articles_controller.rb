dfbda203315b84fd16a053a96d52510105423ea9
tidus1107 <66785728+tidus1107@users.noreply.github.com>
2020-08-11 22:05:37 +0900

    保存機能、validation、日本語化を追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,4 +11,18 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def create
+        @article = Article.new(article_params) #データを保存する箱を作る
+        if @article.save #データを保存する
+            redirect_to article_path(@article),notice: '保存出来ました'
+        else
+            flash.now[:error] = '保存に失敗しました'
+            render :new
+        end
+    end
+
+    private #ストロングパラメーターを使う際はpravateを使う
+    def article_params #更新する対象_paramsで書く
+        params.require(:article).permit(:title, :content) #データ改竄を防ぐために記事形式のタイトルとコンテンツ以外は保存しないようになっている
+    end
 end
