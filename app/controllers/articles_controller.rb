c92de805dc49292f37443d8a5a52db7fd2ad1c3f
tidus1107 <66785728+tidus1107@users.noreply.github.com>
2020-08-19 00:07:57 +0900

    不要なonlyオプションを削除、before_action、日付の表記を動的に、戻るボタンを追加、 部分テンプレートの使用、formを部分テンプレートに変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit ,:update]
+    
     def index
         @articles = Article.all
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def new
@@ -22,11 +23,9 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             redirect_to article_path(@article), notice: '更新できました'
         else
@@ -44,5 +43,9 @@ class ArticlesController < ApplicationController
     private #ストロングパラメーターを使う際はpravateを使う
     def article_params #更新する対象_paramsで書く
         params.require(:article).permit(:title, :content) #データ改竄を防ぐために記事形式のタイトルとコンテンツ以外は保存しないようになっている
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
     end
 end
