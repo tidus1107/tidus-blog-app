ac2ed3e12f610515eb9266daf2ef87fb4882b767
tidus1107 <66785728+tidus1107@users.noreply.github.com>
2020-10-12 00:14:23 +0900

    migrateに関するコメントをメモした

--- db/migrate/20201011144354_remove_content_from_articles.rb
+++ db/migrate/20201011144354_remove_content_from_articles.rb
@@ -2,4 +2,12 @@ class RemoveContentFromArticles < ActiveRecord::Migration[6.0]
   def change
     remove_column :articles, :content, :text
   end
+
+  # def up (migrateしたときに実行される)
+  #   remove_column :articles, :content
+  # end
+
+  # def down (rollbackしたときに実行される)
+  #   add_column :articles, :content, :text
+  # end
 end
