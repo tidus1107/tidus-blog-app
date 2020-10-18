6e50809f93105bc4d122ffdc33c62d69204e2b89
tidus1107 <66785728+tidus1107@users.noreply.github.com>
2020-10-18 22:34:10 +0900

    day8 JSコードのリファクタリング

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -1,8 +1,12 @@
 import $ from 'jquery'
-import axios from 'axios'
-import { csrfToken } from 'rails-ujs'
+import axios from 'modules/axios'
 
-axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+import {
+    listenInactiveHeartEvent,
+    listenActiveHeartEvent
+}  from 'modules/handle_heart'
+
+
 
 const handleHeartDisplay = (hasLiked) => {
     if (hasLiked){
@@ -61,32 +65,9 @@ document.addEventListener('DOMContentLoaded', () => {
           handleHeartDisplay(hasLiked)
       })
 
-    $('.inactive-heart').on('click', () => {
-      axios.post(`/articles/${articleId}/like`)
-        .then((response) => {
-          if (response.data.status ==='ok'){
-            $('.active-heart').removeClass('hidden')
-            $('.inactive-heart').addClass('hidden')
-          }
-        })
-        .catch((e) => {
-            window.alert('Error')
-            console.log(e)
-        })
-    })
-
-    $('.active-heart').on('click', () => {
-        axios.delete(`/articles/${articleId}/like`)
-          .then((response) => {
-            if (response.data.status ==='ok'){
-              $('.inactive-heart').removeClass('hidden')
-              $('.active-heart').addClass('hidden')
-            }
-          })
-          .catch((e) => {
-              window.alert('Error')
-              console.log(e)
-          })
-      })
+    
+    listenInactiveHeartEvent(articleId)
+    listenActiveHeartEvent(articleId)
+    
 
 })
