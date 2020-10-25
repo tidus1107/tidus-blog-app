19c5744002486589bf8a3716ef338a5e1949829f
tidus1107 <66785728+tidus1107@users.noreply.github.com>
2020-10-26 00:21:04 +0900

    多言語化、article#showの一部を英語対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
-
+  before_action :set_local
   def current_user
     ActiveDecorator::Decorator.instance.decorate(super) if super.present?
     super
     # deviceとactiveDecoratorを組み合わせた際に必要になるメソッドだと思っておけば良さそう
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+  
+  private 
+  def set_local
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
